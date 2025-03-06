70cf098d994c6dd26bde6362659ec2b084463f8d
law8cam <law8cam@gmail.com>
2025-03-06 08:39:00 +0000

    intial

---
+++ wp-content/uploads/uag-plugin/assets/2000/uag-js-1605.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-w2agyzob' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-0fkhslxh' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-eu321hz4' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-rdfuiimp' );
+});
+ });
